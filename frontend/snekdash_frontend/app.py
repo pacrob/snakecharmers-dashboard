7ef6ae4bcd2891b5b93717092e51883bfee7d093
pacrob <pacrob@protonmail.com>
2024-01-13 09:46:38 -0700

    fix port, add root button to frontend

--- frontend/snekdash_frontend/app.py
+++ frontend/snekdash_frontend/app.py
@@ -6,10 +6,10 @@ import streamlit as st
 st.title("Data Viewer")
 
 
-def fetch_data_from_backend():
+def fetch_root_data():
     """Fetch data from the backend and returns it as a JSON object."""
     try:
-        response = requests.get("http://backend:5000/newsfragments")
+        response = requests.get("http://backend:8000/")
         print(response)
         return json.dumps({"response": response})
         # return response.json()
@@ -18,11 +18,31 @@ def fetch_data_from_backend():
 
 
 # Displaying data
-if st.button("Fetch Data"):
-    data = fetch_data_from_backend()
+if st.button("Fetch Root Data"):
+    data = fetch_root_data()
     if data:
         st.json(data)
     else:
-        st.error("Failed to fetch data.")
+        st.error("Failed to fetch root data.")
+
+
+def fetch_newsfragment_data():
+    """Fetch data from the backend and returns it as a JSON object."""
+    try:
+        response = requests.get("http://backend:8000/newsfragments")
+        print(response)
+        return json.dumps({"response": response})
+        # return response.json()
+    except requests.RequestException as e:
+        return str(e)
+
+
+# Displaying data
+if st.button("Fetch Newsfragment Data"):
+    data = fetch_newsfragment_data()
+    if data:
+        st.json(data)
+    else:
+        st.error("Failed to fetch newsfragment data.")
 
 # To run the frontend, use: streamlit run app.py
